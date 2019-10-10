1fe1afa2100cedcb483e83babdbecf9280c6ccc7
planning-poker <carmichaelgym.1@gmail.com>
2019-10-09 22:54:20 -0400

    Updates

---
+++ precache-manifest.a9fc60e3e10e95cd15eda6b48250dabf.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3afdc060cb303a3b14b25032d86e4c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87df3d42da90736cf770",
+    "url": "/static/css/2.3957ff16.chunk.css"
+  },
+  {
+    "revision": "0c94a29b1749f07e1292",
+    "url": "/static/css/main.96f54a1e.chunk.css"
+  },
+  {
+    "revision": "87df3d42da90736cf770",
+    "url": "/static/js/2.b4a74287.chunk.js"
+  },
+  {
+    "revision": "0c94a29b1749f07e1292",
+    "url": "/static/js/main.45d80d6c.chunk.js"
+  },
+  {
+    "revision": "f0d41491b4c76a5bff7b",
+    "url": "/static/js/runtime~main.c99bf0a6.js"
+  },
+  {
+    "revision": "e0d2b7486b532288a980d0cd5d535340",
+    "url": "/static/media/chat-icon.e0d2b748.svg"
+  },
+  {
+    "revision": "7718b3e3ff59dac728c55e725a3b0523",
+    "url": "/static/media/logo-no-bg.7718b3e3.svg"
+  },
+  {
+    "revision": "0d062b33a8b60acb2a497afe63666b16",
+    "url": "/static/media/notification.0d062b33.mp3"
+  }
+]);
